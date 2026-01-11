06fe18a6fe89291c29edfe32aba4ee46bc11b636
phwnganh <phuonganh25102003@gmail.com>
2026-01-11 17:22:42 +0700

    fix: spacing between elements of not your avg hero section

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,15 +1,17 @@
 import HeroBannerSection from "./components/HeroBannerSection";
 import OurCommunitySection from "./components/OurCommunitySection";
-import MembershipJourneySection from "./components/MembershipJourneySection";
+import BookClubFeatureSection from "./components/BookClubFeatureSection.jsx";
 import MembershipPriceOptionsSection from "./components/MembershipPriceOptionsSection";
 import FooterSection from "./components/FooterSection";
+import NotYourAverageHeroSection from "./components/NotYourAverageHeroSection.jsx";
 
 const MainPage = () => {
     return (
-        <main>
+        <main className={"flex flex-col gap-16 sm:gap-20 md:gap-30"}>
             <HeroBannerSection/>
+            <BookClubFeatureSection/>
+            <NotYourAverageHeroSection/>
             <OurCommunitySection/>
-            <MembershipJourneySection/>
             <MembershipPriceOptionsSection/>
             <FooterSection/>
         </main>
--- src/components/BookClubFeatureSection.jsx
+++ src/components/BookClubFeatureSection.jsx
@@ -1,7 +1,6 @@
 import ReadTogetherImage from '../assets/images/image-read-together-desktop.webp'
 import CheckIcon from '../assets/images/icon-check.svg'
-import NotEverageImage from '../assets/images/image-not-average-desktop.webp'
-const MembershipJourneySection = () => {
+const BookClubFeatureSection = () => {
     const MEMBERSHIP_JOURNEY_ITEMS = [
         {
             key: 1,
@@ -21,7 +20,7 @@ const MembershipJourneySection = () => {
         }
     ]
     return (
-        <section className={"max-w-292.5 mx-auto mt-30 px-4 sm:px-8 md:px-0"}>
+        <section className={"max-w-292.5 mx-auto px-4 sm:px-8 md:px-0"}>
             <div className={"flex flex-col-reverse md:flex-row items-center gap-500 md:gap-1000"}>
                 <div className={"flex items-center justify-center w-full md:max-w-140"}>
                     <img src={ReadTogetherImage} alt="ReadTogether" className={"rounded-16"}/>
@@ -29,7 +28,7 @@ const MembershipJourneySection = () => {
                 <div className={"flex flex-col gap-400"}>
                     <h2 className={"font-preset-2-font-weight font-preset-text-2-font-family text-preset-2-mobile-font-size sm:text-preset-2-font-size leading-preset-2-line-height tracking-preset-2-letter-spacing text-neutral-900"}>Read together,
                         grow together</h2>
-                    <div className={"flex flex-col gap-6"}>
+                    <div className={"flex flex-col gap-4 sm:gap-6"}>
                         {MEMBERSHIP_JOURNEY_ITEMS.map((item, index) => (
                             <div className={"flex items-center gap-4"}>
                                 <div className={"flex justify-center items-center w-6 h-4"}>
@@ -42,17 +41,9 @@ const MembershipJourneySection = () => {
                 </div>
             </div>
 
-            <div className={"mt-16 sm:mt-20 md:mt-30 flex flex-col md:flex-row items-center gap-500 md:gap-1000"}>
-                <div className={"flex flex-col gap-6"}>
-                        <h2 className={"font-preset-2-font-weight font-preset-text-2-font-family text-preset-2-mobile-font-size sm:text-preset-2-font-size leading-preset-2-line-height tracking-preset-2-letter-spacing text-neutral-900"}>Not your average book <span className={"inline-flex pattern-circle bg-no-repeat bg-contain bg-center px-4 py-2"}>club</span></h2>
-                    <p className={"text-neutral-700 text-preset-5-font-size font-preset-5-font-family font-preset-5-mobile-font-weight sm:font-normal leading-preset-5-line-height tracking-preset-5-letter-spacing"}>Connect with a community that speaks your language - from <span className={"font-preset-5-mobile-font-weight"}>Python</span> to <span className={"font-preset-5-mobile-font-weight"}>TypeScript</span> and everything in between. Our discussions blend technical depth with practical applications.</p>
-                </div>
-                <div className={"flex items-center justify-center w-full md:max-w-140"}>
-                    <img src={NotEverageImage} alt={"not-average-image"} className={"rounded-16"}/>
-                </div>
-            </div>
+
         </section>
     );
 };
 
-export default MembershipJourneySection;
+export default BookClubFeatureSection;
